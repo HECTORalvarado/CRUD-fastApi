8dc71a05ab33ae933a947cc5123fad355ea984f7
Héctor Alvarado <hectoralexanderalvarado@gmail.com>
2023-04-26 17:11:57 -0600

    Se creo el modelo con el que trabajaran los datos de la API

---
+++ model/students.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, Required
+from fastapi import Query, Path
+from typing import Annotated
+
+class Student (BaseModel):
+    id: Annotated[int, None, Query(
+        title= "ID",
+        description= "ID del estudiante")]
+    name: Annotated[str, Query(
+        title= "Nombre",
+        description= "Nombre del estudiante",
+		max_length=75)] = Required
+    lastName: Annotated[str, Query(
+        title= "Apellido",
+        description= "Apellido del estudiante",
+		max_length=100)] = Required
+    habilitado: Annotated[int, Path(
+        title= "Habilitado",
+        description= "Estado del estudiante, puede ser 1 o 0",
+		max_length=1,
+        ge= 0,
+        le= 1
+        )] = 0
+    email: Annotated[str, Query(
+        title= "Correo electronico",
+        description= "Correo electronico del estudiante",
+		regex=r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')] = Required
+    date: Annotated[str, Query(
+        title= "Fecha de nacimiento",
+        description= "Fecha de nacimiento del estudiante",
+		regex=r'^\d{2}-\d{2}-\d{4}$')] = Required
+    phone: Annotated[str, Query(
+        title= "Telefono",
+        description= "Telefono del estudiante",
+		regex=r'^\d{4}-\d{4}$')] = Required
