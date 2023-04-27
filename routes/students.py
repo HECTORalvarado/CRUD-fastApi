631988b7949bca4957eda24f05437b333870b22c
Héctor Alvarado <hectoralexanderalvarado@gmail.com>
2023-04-26 21:14:28 -0600

    Se solucionaron errores debido a los cambios en la estructura de las carpetas

--- routes/students.py
+++ routes/students.py
@@ -1,12 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from schemas.students import Student
 
 students = APIRouter()
 
-@students.get("/students")
+@students.get("/students", status_code= status.HTTP_200_OK)
 def getStudents():
     return "Students"
 
 
-@students.get("/students/{student_id}")
+@students.get("/students/{student_id}", status_code= status.HTTP_200_OK)
 def getStudent(student_id : int):
+    return "Students", student_id
+
+@students.post("/students/", status_code= status.HTTP_201_CREATED)
+def createStudent( student: Student):
+    return 201
+
+@students.put("/students/{student_id}", status_code= status.HTTP_200_OK)
+def updateStudent(student_id : int):
+    return "Students", student_id
+
+@students.delete("/students/{student_id}", status_code= status.HTTP_200_OK)
+def deleteStudent(student_id : int):
     return "Students", student_id
